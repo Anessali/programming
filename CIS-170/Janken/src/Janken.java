178a3da17f9cfc74a6a1bb83dc3e861f2dcce4de
shironya <liggettt@otc.edu>
2017-09-06 20:57:54 -0500

    updates to Janken

--- CIS-170/Janken/src/Janken.java
+++ CIS-170/Janken/src/Janken.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Janken {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
         // declared variables
         Scanner input = new Scanner(System.in);
         String cNum = ""; //computer string
         String uNum = ""; //user string
         int compNum = (int) (Math.random() * 3);
         //prompts for user input
-        System.out.println("scissor (0), rock (1), paper (2): ");
+        System.out.print("scissor (0), rock (1), paper (2): ");
         int userNum = input.nextInt();
         //data processing
         switch (compNum) {
             case 0:
-                cNum = "The computer is scissors. ";
+                cNum = "The computer is scissor. ";
                 break;
             case 1:
                 cNum = "The computer is rock. ";
@@ -25,7 +25,7 @@ public static void main(String[] args) {
         }
         switch (userNum) {
             case 0:
-                uNum = "You are scissors. ";
+                uNum = "You are scissor. ";
                 break;
             case 1:
                 uNum = "You are rock. ";
@@ -41,27 +41,27 @@ public static void main(String[] args) {
         * 2 = paper
         * */
         if (compNum == userNum) {
-            System.out.println(cNum + uNum + "It's a tie.");
+            System.out.print(cNum + uNum + "It's a tie");
         }
         else if(compNum == 0 && userNum == 1){
-            System.out.println(cNum + uNum + "You won.");
+            System.out.print(cNum + uNum + "You won");
         }
         else if(compNum == 0 && userNum == 2){
-            System.out.println(cNum + uNum + "You lost.");
+            System.out.print(cNum + uNum + "You lost");
         }
         else if(compNum == 1 && userNum == 0){
-            System.out.println(cNum + uNum + "You lost.");
+            System.out.print(cNum + uNum + "You lost");
         }
         else if(compNum == 1 && userNum == 2){
-            System.out.println(cNum + uNum + "You won.");
+            System.out.print(cNum + uNum + "You won");
         }
         else if(compNum == 2 && userNum == 0){
-            System.out.println(cNum + uNum + "You won.");
+            System.out.print(cNum + uNum + "You won");
         }
         else if(compNum == 2 && userNum == 1){
-            System.out.println(cNum + uNum + "You lost.");
+            System.out.print(cNum + uNum + "You lost");
         } else {
-            System.out.println("Error. Please enter a valid number.");
+            System.out.print("Error. Please enter a valid number.");
         }
     }
 }
