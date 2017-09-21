5528d33ed5d939c669df8a7cfef1ac7cbfc41f12
liggettt <liggettt@otc.edu>
2017-09-21 13:07:18 -0500

    170 Chapter 6 start

---
+++ CIS-170/Ex63/src/Ex63.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Ex63 {
+
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int num = input.nextInt();
+    }
+    
+    //should return the reverse of passed value
+    public static int reverse(int number){
+        number = 10;
+        return number;
+    }
+    //return true if number is palindrome(ie, if number is same backwards)
+    public static boolean isPalindrome(int number){
+        boolean beluga = true;
+        System.out.println("If this is working, then your number is " + number);
+        return beluga;
+    }
+}
