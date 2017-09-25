a4ef0d6c8ec4d6383b17d66ea5a946c3e8b74d79
liggettt <liggettt@otc.edu>
2017-09-25 12:07:52 -0500

    Started weight converter

---
+++ CIS-151/WeightConvert/Ex12-6WeightConv/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Ex12_6WeightConv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTempIn = new System.Windows.Forms.TextBox();
+            this.txtTempOut = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 143);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtTempIn
+            // 
+            this.txtTempIn.Location = new System.Drawing.Point(140, 100);
+            this.txtTempIn.Name = "txtTempIn";
+            this.txtTempIn.Size = new System.Drawing.Size(112, 24);
+            this.txtTempIn.TabIndex = 1;
+            // 
+            // txtTempOut
+            // 
+            this.txtTempOut.Location = new System.Drawing.Point(36, 202);
+            this.txtTempOut.Name = "txtTempOut";
+            this.txtTempOut.ReadOnly = true;
+            this.txtTempOut.Size = new System.Drawing.Size(324, 24);
+            this.txtTempOut.TabIndex = 2;
+            this.txtTempOut.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Temperature";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "grams",
+            "kilograms",
+            "pounds",
+            "ounces"});
+            this.comboBox1.Location = new System.Drawing.Point(239, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "ounces";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To...";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // cbBox
+            // 
+            this.cbBox.FormattingEnabled = true;
+            this.cbBox.Items.AddRange(new object[] {
+            "grams",
+            "kilograms",
+            "pounds",
+            "ounces"});
+            this.cbBox.Location = new System.Drawing.Point(27, 43);
+            this.cbBox.Name = "cbBox";
+            this.cbBox.Size = new System.Drawing.Size(121, 26);
+            this.cbBox.TabIndex = 6;
+            this.cbBox.Text = "grams";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 274);
+            this.Controls.Add(this.cbBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTempOut);
+            this.Controls.Add(this.txtTempIn);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTempIn;
+        private System.Windows.Forms.TextBox txtTempOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbBox;
+    }
+}
+
