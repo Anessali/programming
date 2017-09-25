e7d7ca77aee836d09a3bf4f812d811d24b67996c
Unknown <liggettt@otc.edu>
2017-09-24 21:50:10 -0500

    Finished Ex63

--- CIS-170/Ex63/src/Ex63.java
+++ CIS-170/Ex63/src/Ex63.java
@@ -4,19 +4,30 @@ public class Ex63 {
     
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter a number: ");
+        System.out.print("Enter a number to test if it's a palindrome: ");
         int num = input.nextInt();
+        reverse(num);
     }
     
     //should return the reverse of passed value
     public static int reverse(int number){
-        number = 10;
-        return number;
+        boolean p;
+        int oldNum = number;
+        int reverse = 0;
+        while (number != 0){
+            reverse = reverse * 10 + number % 10;
+            number = number / 10;
+        }
+        p = isPalindrome(oldNum, reverse);
+        System.out.println(p);
+        return oldNum;
     }
-    //return true if number is palindrome(ie, if number is same backwards)
-    public static boolean isPalindrome(int number){
-        boolean beluga = true;
-        System.out.println("If this is working, then your number is " + number);
-        return beluga;
+    //return true if number is same backwards)
+    public static boolean isPalindrome(int old, int rev){
+        boolean palindrome = true;
+        if(old != rev){
+            palindrome = false;
+        }
+        return palindrome;
     }
 }
