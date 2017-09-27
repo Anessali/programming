90ca3caa2e38bb7802f6336e5be06611966182d5
liggettt <liggettt@otc.edu>
2017-09-27 15:14:28 -0500

    Commit 3:14 9/27/2017

--- CIS-151/6-1BubbleSort/BubbleSort/Program.cs
+++ CIS-151/6-1BubbleSort/BubbleSort/Program.cs
@@ -10,8 +10,13 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
+            //declared variables
             int[] myArray = new int[] { 100, 20, 1, 600, 300, 33, 71, 92 };
 
+
+            /* ****************************************** *
+             * Counts array before it is sorted by bubble *
+             * ****************************************** */
             for (int i = 0; i < myArray.Length; i++)
             {
                 Console.WriteLine(myArray[i]);
