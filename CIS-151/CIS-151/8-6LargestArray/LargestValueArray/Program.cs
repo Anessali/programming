86f8c2555488e39eea007cde625711dabc3c91b1
Shiro <liggetttj@gmail.com>
2017-10-01 22:24:43 -0500

    started LargestValueArray

---
+++ CIS-151/CIS-151/8-6LargestArray/LargestValueArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Application randomly generates
+ * numbers in a two-dimensional 
+ * array
+ */
+namespace LargestValueArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows;
+            int columns;
+            int randomNum;
+            bool exitVar = false;
+            while (exitVar == false)
+            {
+                try
+                {   
+                    Console.WriteLine("Enter number of rows: ");
+                    rows = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter number of columns: ");
+                    columns = Convert.ToInt32(Console.ReadLine());
+                    int[,] randArray = new int[rows, columns];
+                    Console.WriteLine(randArray[10, 0]);
+
+                    exitVar = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
