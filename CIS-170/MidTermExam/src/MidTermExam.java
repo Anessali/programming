3e932e3a1c6ff7eb182f6fae03bf0b9192514ef0
Shiro <shiro@localhost.localdomain>
2017-10-06 22:25:25 -0500

    final edits for tonight

--- CIS-170/MidTermExam/src/MidTermExam.java
+++ CIS-170/MidTermExam/src/MidTermExam.java
@@ -4,42 +4,80 @@ public class MidTermExam {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         long credNum = 0;
-        long numMod; //will hold last number in long
-        long numDouble; //holds doubled digits
-        int credLength; //represents # of doubled digits
+        int numLength,
+            credLength, //represents # of doubled digits
+            numDouble, //holds doubled digits
+            numMod, //will hold last number in long
+            oddNum; //holds odd numbers
+        
         //user input
         System.out.print("Enter a credit card number: ");
         credNum = input.nextLong();
-        
         /*************************************************************
-         * credNum is converted to string to get length divided by 2.*
+         * credNum is converted to string to get length divided by 2 *
          * credLength = the number of values being doubled           *
-         * numHolder array is used to hold doubled numbers           *
          *************************************************************/
         credLength = Long.toString(credNum).length() / 2;
         int numHolder[] = new int[credLength]; 
-        System.out.println("Cred length: " + credLength);
-        /******************************************************************************
-         * numMod collects the second digit from the right. The rest of the math      * 
-         * further breaks down the number to reach every second digit.                *
-         ******************************************************************************/
+        int oddHolder[] = new int[credLength];
         for (int i = 0; i < credLength; i++) {
-            numMod = ((credNum - (credNum % 10)) / 10) % 10;
+            //collects second digit from right
+            numMod = (int) (((credNum - (credNum % 10)) / 10) % 10);
+            //odd number stored
+            oddNum = (int) (credNum % 10);
+            //removes last two digits from credNum
+            credNum = (credNum - credNum % 10) / 10;
+            credNum = (credNum - credNum % 10) / 10;
+            //doubles collected number
             numDouble = numMod * 2;
-            System.out.println("Doubled number: " + numDouble);
+            //double digit number's digits are added up
+            numLength = Long.toString(numDouble).length();
+            if (numLength > 1){
+                numDouble = getDigit(numLength, numDouble);
+            }
+            //numbers stored in array
+            numHolder[i] = numDouble;
+            oddHolder[i] = oddNum;
         }
+        
+        int evenSum = sumOfDoubleEvenPlace(numHolder);
+        int oddSum = sumOfOddPlace(oddHolder);
+        boolean isItValid = isValid(evenSum, oddSum);
+        
+        
         
     }//end of main
     
-//    public static boolean isValid(long credNum){
-//        boolean creditCard;
-//        return creditCard;
-//    }
+    public static boolean isValid(long credNum){
+        boolean creditCard;
+        
+        return creditCard;
+    }
     
-//    public static int sumOfDoubleEvenPlace(){
-//        
-//    }
+    public static int sumOfDoubleEvenPlace(int[] evenNums){
+        int numSum = 0;
+        for(int i = 0; i < evenNums.length; i++){
+            numSum = numSum + evenNums[i];
+        }
+        return numSum;
+    }
     
+    public static int sumOfOddPlace(int[] oddNums){
+        int numSum = 0;
+        for(int i = 0; i < oddNums.length; i++){
+            numSum = numSum + oddNums[i];
+        }
+        return numSum;
+    }
     
-    
+    //10 = 3 (1 + 2)
+    public static int getDigit(int numLength, int num){
+        int numOne, numTwo;
+        for (int x = 0; x < numLength; x++){
+            numOne = num % 10;
+            numTwo = (num - numOne) / 10;
+            num = numOne + numTwo;
+        }
+        return num;
+    }
 }
