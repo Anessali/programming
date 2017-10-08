b99dcc7eee0ef681b7a337cfbd8bff89a4f650b5
ShiroNya <liggetttj@gmail.com>
2017-10-07 21:14:41 -0500

    some edits

--- CIS-151/7-1BucketSort/BucketSort/BucketSort.cs
+++ CIS-151/7-1BucketSort/BucketSort/BucketSort.cs
@@ -8,9 +8,31 @@ namespace BucketSort
 {
     class BucketSort
     {
-        //public Sort()
-        //{
+        private int[] nums;
+        private int[,] bucket;
 
-        //}
-    }
+        public BucketSort(int[] nums, int[,] bucket)
+        {
+            this.nums = nums;
+            this.bucket = bucket;
+        }
+        
+        public static void Sort()
+        {
+
+        }
+
+        #region getters/setters
+        public int[] Nums
+        {
+            get { return nums; }
+            set { nums = value; }
+        }
+        public int[,] Bucket
+        {
+            get { return bucket; }
+            set { bucket = value; }
+        }
+        #endregion
+    }//end of BucketSort class
 }
