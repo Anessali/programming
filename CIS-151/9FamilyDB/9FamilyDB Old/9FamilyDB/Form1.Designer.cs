27eb56011cc450d249b0b909ebc7a01ae454e252
Shiro <liggetttj@gmail.com>
2017-10-26 18:56:12 -0500

    commit

---
+++ CIS-151/9FamilyDB/9FamilyDB Old/9FamilyDB/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace _9FamilyDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lstFamily = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(12, 470);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 20);
+            this.lblMessage.TabIndex = 1;
+            // 
+            // lstFamily
+            // 
+            this.lstFamily.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstFamily.FormattingEnabled = true;
+            this.lstFamily.ItemHeight = 20;
+            this.lstFamily.Location = new System.Drawing.Point(0, 0);
+            this.lstFamily.Name = "lstFamily";
+            this.lstFamily.Size = new System.Drawing.Size(322, 318);
+            this.lstFamily.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 318);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lstFamily);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Family Database";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.ListBox lstFamily;
+    }
+}
+
---
+++ CIS-151/9FamilyDB/9FamilyDB Old/9FamilyDB/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+/* **************************
+ * This program pulls data  *
+ * from the access database.*
+ * Similar to what we did   *
+ * in class.                *
+ * ************************ */
+
+namespace _9FamilyDB
+{
+    public partial class Form1 : Form
+    {
+        //sets up connection
+        private OleDbConnection dbConnection;   
+        private string sConnection;             
+                                                
+        //holds command                         
+        private OleDbCommand dbCmd;             
+        private string sql;                     
+                                                
+        //sets up reader                        
+        private OleDbDataReader familyReader;
+        private Family familyObj;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                sConnection =
+                    "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                    "Data Source=familyDB.accdb;";
+                dbConnection = new OleDbConnection(sConnection);
+                dbConnection.Open();
+
+                //build our sql statement
+                sql = "SELECT * FROM familyTable " +
+                        "ORDER BY LastName ASC, FirstName ASC;";
+
+                //instance of cmd is created
+                dbCmd = new OleDbCommand();     
+                dbCmd.CommandText = sql;
+                dbCmd.Connection = dbConnection;
+                //create reader object
+                familyReader = dbCmd.ExecuteReader();
+                while (familyReader.Read())
+                {
+                    familyObj = new Family(familyReader["FirstName"].ToString(),
+                        familyReader["LastName"].ToString(),
+                        familyReader["Relationship"].ToString(),
+                        familyReader["Hometown"].ToString()
+                        );
+                    lstFamily.Items.Add(familyObj);
+                }
+                familyReader.Close();
+            }
+            catch (System.Exception exc)
+            {
+                lblMessage.Text = exc.Message;
+            }
+        }
+    }
+}
