e3821af614c2de027c4e2cb4108227e70a5703ef
liggettt <liggettt@otc.edu>
2017-11-07 15:08:18 -0600

    Kirsten's class

---
+++ CIS-170/Watch/src/watch/Stopwatch.java
@@ -0,0 +1,42 @@
+
+package watch;
+
+public class Stopwatch {
+    //private variables
+    private int startTime;
+    private int endTime;
+    
+    //constructors
+    
+    
+    public static int start(int startTime){
+        startTime = 0;
+        return startTime;
+    }
+    
+    public static int stop(int endTime){
+        endTime = 0;
+        return endTime;
+    }
+    
+    public static int getElapsedTime(){
+        int elapsed = 0;
+        return elapsed;
+    }
+    
+    //getter startTime
+    public int getStart(){
+        return startTime;
+    }
+    //setter startTime
+    public void setStart(int startTime){
+        this.startTime = 0;
+    }
+    //getter/setter for endTime
+    public int getEnd(){
+        return endTime;
+    }
+    public void setEnd(int endTime){
+        this.endTime = ;
+    }
+} 
---
+++ CIS-170/Watch/src/watch/Watch.java
@@ -0,0 +1,10 @@
+
+package watch;
+
+public class Watch {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
